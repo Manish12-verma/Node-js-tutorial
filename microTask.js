54f4081f3d0f110fe25fecca48bbc4247c23adf2
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-05-03 11:54:08 +0530

    Learned microTask queues

---
+++ microTask.js
@@ -0,0 +1,10 @@
+// console.log('console.log1');
+// process.nextTick(()=>console.log('this is process.next 1'));
+// console.log('console.log2');
+
+
+// Promise.resolve().then(()=>{
+//     console.log("this is Promise.resolve 1");
+// })
+// process.nextTick(()=> console.log("this is process.nextTick 1"));
+
